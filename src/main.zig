5d54c961e7f4032b8041f33f9ae192bb1e85b1bd
Ethan Zhang <ezsland05@gmail.com>
2022-07-01 16:09:08 -0700

    Added a sphere to the scene!

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,35 @@ const Color = color.Color;
 const Ray = @import("ray.zig");
 const Point3 = Ray.Point3;
 
+fn hitSphere(center: *const Point3, radius: f64, ray: *const Ray) bool {
+    // A sphere is given by the equation: $\left( x - C_x \right)^2 + \left( y - C_y \right)^2 + \left( z - C_z \right)^2 = r^2$.
+    // Written in vector form, we get: $\left(\textbf{P} - \textbf{C}\right) \cdot \left(\textbf{P} - \textbf{C}\right) = r^2$.
+    // Thus, any point P that satisfies this equation is on the sphere. We want to know if our Ray $\textbf{P}(t) = \textbf{A} + t\textbf{b}$
+    // ever hits this sphere, so all we really need to do is solve for if there exists a $t$ such that
+    // $\left(\textbf{P}(t) - \textbf{C}\right) \cdot \left(\textbf{P}(t) - \textbf{C}\right) = r^2$ is true.
+    // Expanding, we get the equation:
+    // $t^{2}\textbf{b} \cdot \textbf{b} + 2t\textbf{b} \cdot \left(\textbf{A} - \textbf{C}\right) + \left(\textbf{A} - \textbf{C}\right) \cdot \left(\textbf{A} - \textbf{C}\right) - r^{2} = 0$
+    // This is simply a quadrtic function, and based on the discriminant, we can determine if the ray interesects the sphere at 0, 1, or 2 points.
+
+    // $\left(\textbf{A} - \textbf{C}\right)$
+    const origin_to_center = ray.getOrigin().subtract(center);
+
+    // $a = t^{2}\textbf{b} \cdot \textbf{b}$
+    const a = Vec3.dot(&ray.getDirection(), &ray.getDirection());
+    // $b = 2t\textbf{b} \cdot \left(\textbf{A} - \textbf{C}\right)$
+    const b = 2.0 * Vec3.dot(&ray.getDirection(), &origin_to_center);
+    // $c = \left(\textbf{A} - \textbf{C}\right) \cdot \left(\textbf{A} - \textbf{C}\right) - r^{2}$
+    const c = Vec3.dot(&origin_to_center, &origin_to_center) - radius * radius;
+
+    const discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
+}
+
 fn rayColor(r: *const Ray) Color {
+    // if this ray hits the sphere, color it red
+    if (hitSphere(&Point3.init(0.0, 0.0, -1.0), 0.5, r))
+        return Color.init(1.0, 0.0, 0.0);
+
     // scale the ray direction to unit length (-1.0 < y < 1.0)
     const unit_direction = Vec3.normalize(&r.getDirection());
 
